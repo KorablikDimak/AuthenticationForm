14cc061c02de952c6817cd06a583c9c2f93f7ff9
korab <korablikdimak@gmail.com>
2022-05-25 16:59:49 +0300

    refact

---
+++ AuthenticationEmbedder/Models/EmailModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationEmbedder.Models;
+
+public class EmailModel
+{
+    public int Id { get; set; }
+    [Required] public string Email { get; set; }
+    [Required] public string Token { get; set; }
+    [Required] public string ResponseAddress { get; set; }
+    [Required] public DateTime DateTime { get; set; }
+}
